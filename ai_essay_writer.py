705e0d2736b6560ab58c1700c06866ac6fd2bf60
AjaySi <ajay.calsoft@gmail.com>
2024-04-14 20:48:41 +0530

    Alwrity AI Essay writer

---
+++ ai_essay_writer.py
@@ -0,0 +1,183 @@
+#####################################################
+#
+# Alwrity, AI essay writer - Essay_Writing_with_Prompt_Chaining
+#
+#####################################################
+
+import os
+from google.api_core import retry
+import google.generativeai as genai
+from pprint import pprint
+
+
+def generate_with_retry(model, prompt):
+    """
+    Generates content from the model with retry handling for errors.
+
+    Parameters:
+        model (GenerativeModel): The generative model to use for content generation.
+        prompt (str): The prompt to generate content from.
+
+    Returns:
+        str: The generated content.
+    """
+    try:
+        # FIXME: Need a progress bar here.
+        return model.generate_content(prompt, request_options={'retry':retry.Retry()})
+    except Exception as e:
+        print(f"Error generating content: {e}")
+        return ""
+
+
+def ai_essay_generator(essay_title, selected_essay_type, selected_education_level, selected_num_pages):
+    """
+    Write an Essay using prompt chaining and iterative generation.
+
+    Parameters:
+        persona (str): The persona statement for the author.
+        story_genre (str): The genre of the story.
+        characters (str): The characters in the story.
+    """
+    print(f"Starting to write Essay on {essay_title}..")
+    try:
+        # Define persona and writing guidelines
+        guidelines = f'''\
+        Writing Guidelines
+
+        As an expert Essay writer and academic researcher, demostrate your world class essay writing skills.
+        
+        Follow the below writing guidelines for writing your essay:
+        1). You specialize in {selected_essay_type} essay writing.
+        2). Your target audiences include readers from {selected_education_level} level.
+        3). The title of the essay is {essay_title}.
+        4). I will provide you with web research for essay title.
+        5). The final essay should of {selected_num_pages} words/pages.
+        3). Plant the seeds of subplots or potential character arc shifts that can be expanded later.
+
+        Remember, your main goal is to write as much as you can. If you get through
+        the story too fast, that is bad. Expand, never summarize.
+        '''
+        # Generate prompts
+        premise_prompt = f'''\
+        As an expert essay writer, specilizing in {selected_essay_type} essay writing.
+
+        Write an Essay title for given keywords {essay_title}. 
+        The title should appeal to audience level of {selected_education_level}.
+        '''
+
+        outline_prompt = f'''\
+        As an expert essay writer, specilizing in {selected_essay_type} essay writing.
+
+        Your Essay title is:
+
+        {{premise}}
+
+        Write an outline for the essay.
+        '''
+
+        starting_prompt = f'''\
+        As an expert essay writer, specilizing in {selected_essay_type} essay writing.
+
+        Your essay title is:
+
+        {{premise}}
+
+        The outline of the Essay is:
+
+        {{outline}}
+
+        First, silently review the outline and the essay title. Consider how to start the Essay.
+        Start to write the very beginning of the Essay. You are not expected to finish
+        the whole Essay now. Your writing should be detailed enough that you are only
+        scratching the surface of the first bullet of your outline. Try to write AT
+        MINIMUM 1000 WORDS.
+
+        {guidelines}
+        '''
+
+        continuation_prompt = f'''\
+        As an expert essay writer, specilizing in {selected_essay_type} essay writing.
+
+        Your essay title is:
+
+        {{premise}}
+
+        The outline of the Essay is:
+
+        {{outline}}
+
+        You've begun to write the essay and continue to do so.
+        Here's what you've written so far:
+
+        {{story_text}}
+
+        =====
+
+        First, silently review the outline and essay so far. 
+        Identify what the single next part of your outline you should write.
+
+        Your task is to continue where you left off and write the next part of the Essay.
+        You are not expected to finish the whole essay now. Your writing should be
+        detailed enough that you are only scratching the surface of the next part of
+        your outline. Try to write AT MINIMUM 1000 WORDS. However, only once the essay
+        is COMPLETELY finished, write IAMDONE. Remember, do NOT write a whole chapter
+        right now.
+
+        {guidelines}
+        '''
+
+        # Configure generative AI
+        genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+        # Initialize the generative model
+        model = genai.GenerativeModel('gemini-1.0-pro')
+
+        # Generate prompts
+        try:
+            premise = generate_with_retry(model, premise_prompt).text
+            print(f"The title of the Essay is: {premise}")
+        except Exception as err:
+            print(f"Essay title Generation Error: {err}")
+            return
+
+        outline = generate_with_retry(model, outline_prompt.format(premise=premise)).text
+        print(f"The Outline of the essay is: {outline}\n\n")
+        if not outline:
+            print("Failed to generate Essay outline. Exiting...")
+            return
+
+        try:
+            starting_draft = generate_with_retry(model, 
+                    starting_prompt.format(premise=premise, outline=outline)).text
+            pprint(starting_draft)
+        except Exception as err:
+            print(f"Failed to Generate Essay draft: {err}")
+            return
+
+        try:
+            draft = starting_draft
+            continuation = generate_with_retry(model, 
+                    continuation_prompt.format(premise=premise, outline=outline, story_text=draft)).text
+            pprint(continuation)
+        except Exception as err:
+            print(f"Failed to write the initial draft: {err}")
+
+        # Add the continuation to the initial draft, keep building the story until we see 'IAMDONE'
+        try:
+            draft += '\n\n' + continuation
+        except Exception as err:
+            print(f"Failed as: {err} and {continuation}")
+        while 'IAMDONE' not in continuation:
+            try:
+                continuation = generate_with_retry(model, 
+                        continuation_prompt.format(premise=premise, outline=outline, story_text=draft)).text
+                draft += '\n\n' + continuation
+            except Exception as err:
+                print(f"Failed to continually write the Essay: {err}")
+                return
+
+        # Remove 'IAMDONE' and print the final story
+        final = draft.replace('IAMDONE', '').strip()
+        return(final)
+
+    except Exception as e:
+        print(f"Main Essay writing: An error occurred: {e}")
---
+++ essay_writer.py
@@ -0,0 +1,169 @@
+import time
+import os
+import json
+import openai
+import streamlit as st
+
+from ai_essay_writer import ai_essay_generator
+
+
+def main():
+    set_page_config()
+    custom_css()
+    hide_elements()
+    sidebar()
+    title_and_description()
+    input_section()
+
+def set_page_config():
+    st.set_page_config(
+        page_title="Alwrity",
+        layout="wide",
+        page_icon="img/logo.png"
+    )
+
+def custom_css():
+    st.markdown("""
+        <style>
+            .block-container {
+                padding-top: 0rem;
+                padding-bottom: 0rem;
+                padding-left: 1rem;
+                padding-right: 1rem;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+        <style>
+            [class="st-emotion-cache-7ym5gk ef3psqc12"] {
+                display: inline-block;
+                padding: 5px 20px;
+                background-color: #4681f4;
+                color: #FBFFFF;
+                width: 300px;
+                height: 35px;
+                text-align: center;
+                text-decoration: none;
+                font-size: 16px;
+                border-radius: 8px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+def hide_elements():
+    hide_decoration_bar_style = '<style>header {visibility: hidden;}</style>'
+    st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+    hide_streamlit_footer = '<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;}</style>'
+    st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+
+def sidebar():
+    st.sidebar.image("img/alwrity.jpeg", use_column_width=True)
+    st.sidebar.markdown("🧕 :red[Checkout Alwrity], complete **AI writer & Blogging solution**:[Alwrity](https://alwrity.netlify.app)")
+
+
+def title_and_description():
+    st.title("✍️ Alwrity - AI Essay Writer")
+    with st.expander("What is **AI Essay writer** & **How to Use**? 📝❗"):
+        st.markdown('''
+        Choose the inputs carefully and as per your requirements. Below are the options for the inputs.
+        ## Essay Types and Education Levels
+
+		### Essay Types
+		- **Argumentative**: Forming an opinion via research. Building an evidence-based argument.
+		- **Expository**: Knowledge of a topic. Communicating information clearly.
+		- **Narrative**: Creative language use. Presenting a compelling narrative.
+		- **Descriptive**: Creative language use. Describing sensory details.
+		
+		### Education Levels
+		- **Primary School**
+		- **High School**
+		- **College**
+		- **Graduate School**
+		-----------------------------
+
+		## Inputs and Details
+		
+		### Essay Type
+		Choose the type of essay you want to write from the options provided.
+		
+		### Education Level
+		Select your current level of education from the given choices.
+		
+		### Essay Title
+		Enter the title of your essay. The title should accurately reflect the content and focus of your essay.
+		
+		### Number of Pages
+		Select the length of your essay by choosing one or more options:
+		- **Short Form (1-2 pages)**
+		- **Medium Form (3-5 pages)**
+		- **Long Form (6+ pages)**
+            ---
+        ''')
+
+
+def input_section():
+    with st.expander("**PRO-TIP** - Choose your inputs carefully", expanded=True):
+        col1, space, col2 = st.columns([5, 0.1, 5])
+        essay_types = [
+            ("Argumentative", "Argumentative - Forming an opinion via research. Building an evidence-based argument."),
+            ("Expository", "Expository - Knowledge of a topic. Communicating information clearly."),
+            ("Narrative", "Narrative - Creative language use. Presenting a compelling narrative."),
+            ("Descriptive", "Descriptive - Creative language use. Describing sensory details.")
+        ]
+
+        education_levels = [
+            ("Primary School", "Primary School"),
+            ("High School", "High School"),
+            ("College", "College"),
+            ("Graduate School", "Graduate School")
+        ]
+        with col1:
+            # Ask the user for type of essay, level of education, and number of pages
+            selected_essay_type = st.selectbox("Choose the type of essay you want to write:", options=[option[0] for option in essay_types])
+            selected_education_level = st.selectbox("Choose your level of education:", options=[option[0] for option in education_levels])
+
+        with col2:
+            # Ask the user for the title of the essay
+            essay_title = st.text_input("Enter the title of your essay:")
+
+            # Ask the user for the number of pages
+            num_pages_options = ["Short Form (1-2 pages)", "Medium Form (3-5 pages)", "Long Form (6+ pages)"]
+            selected_num_pages = st.selectbox("Select the length of your essay (choose one or more):", options=num_pages_options)
+
+
+        if st.button('AI, Write Essay'):
+            if essay_title.strip():
+                with st.spinner("Generating Essay...💥💥"):
+                    essay_content = ai_essay_generator(essay_title, selected_essay_type, 
+                            selected_education_level, selected_num_pages) 
+                    if essay_content:
+                        st.subheader('**👩🔬👩🔬 Your Awesome Essay:**')
+                        st.markdown(essay_content)
+                    else:
+                        st.error("💥 **Failed to generate Essay. Please try again!**")
+            else:
+                st.error("Essay Title is needed, it needs to be 3 words long !")
+
+    page_bottom()
+
+
+def page_bottom():
+    """Display the bottom section of the web app."""
+
+    st.markdown('''
+    AI Essay Generator, Powered by AI (Gemini Pro).
+
+    Implemented by [Alwrity](https://alwrity.netlify.app).
+
+    ''')
+
+    st.markdown("""
+    ### Free & No Signup. Open Source AI Essay Writer.
+
+    """)
+
+if __name__ == "__main__":
+    main()
+
